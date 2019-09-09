a8bd306e31409601a1dad5e35b3fe1bcf9d3db2d
Patrick <patrick.tschorn@oliverwyman.com>
2019-09-09 18:07:44 +0100

    made publishable by hex

--- mix.exs
+++ mix.exs
@@ -6,24 +6,36 @@ defmodule Arfficionado.MixProject do
       app: :arfficionado,
       version: "0.1.0",
       elixir: "~> 1.8",
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/patrick7777776/arfficionado",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
+
+  defp description() do
+    "Arfficionado: reader for ARFF data sets (Attribute Relation File Format)."
+  end
+
+  defp package() do
+    [
+      name: "arfficionado",
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/patrick7777776/arfficionado"}
+    ]
+  end
+
 end
